b41ccdfce3186a29afc09d64302005fdc06e2f89
robsonazevedo822 <spveiok@hotmail.com>
2020-03-14 21:54:10 -0300

    alteração1

---
+++ __init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+from flask import Flask, render_template 
+
+app = Flask(__name__, static_folder="public")
+
+@app.route("/")
+def index():
+
+    return  render_template('index.html')  
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
